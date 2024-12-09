3949eec036c3bb0378daad6e05a5a0ca711eb299
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-12-09 17:14:48 +0700

    Enhance CartSlice: update total price calculation, implement removeItem and updateQuantity reducers

--- src/state/CartSlice.jsx
+++ src/state/CartSlice.jsx
@@ -13,7 +13,7 @@ export const CartSlice = createSlice({
       const itemInCart = state.cart.find((item) => item.name == action.payload.name);
       if (itemInCart) {
         itemInCart.quantity++;
-        itemInCart.totalPricePerItem = parseInt(action.payload.cost.slice(1) * itemInCart.quantity);
+        itemInCart.totalPricePerItem = '$' + parseInt(action.payload.cost.slice(1) * itemInCart.quantity);
       } else {
         state.cart.push({
           ...action.payload,
@@ -22,8 +22,17 @@ export const CartSlice = createSlice({
         });
       }
     },
-    removeItem: (state, action) => {},
-    updateQuantity: (state, action) => {},
+    removeItem: (state, action) => {
+      const itemIndex = state.cart.findIndex((item) => item.name === action.payload.name);
+      state.cart.splice(itemIndex, 1);
+    },
+    updateQuantity: (state, action) => {
+      const itemToUpdate = state.cart.find((item) => item.name === action.payload.name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity--;
+        itemToUpdate.totalPricePerItem = '$' + (parseInt(itemToUpdate.totalPricePerItem.slice(1)) - parseInt(itemToUpdate.cost.slice(1)));
+      }
+    },
   },
 });
 
