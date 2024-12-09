08c71d874b76c29c8e3db6590334ec97e1ae4adb
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-12-09 15:13:26 +0700

    Refactor CartSlice: update initial state structure and enhance addItem reducer logic

--- src/state/CartSlice.jsx
+++ src/state/CartSlice.jsx
@@ -1,20 +1,29 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+};
 
 export const CartSlice = createSlice({
   name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
+  initialState,
   reducers: {
     addItem: (state, action) => {
-    
+      const itemInCart = state.cart.find((item) => item.name == action.payload.name);
+      if (itemInCart) {
+        itemInCart.quantity++;
+        itemInCart.totalPricePerItem = parseInt(action.payload.cost.slice(1) * itemInCart.quantity);
+      } else {
+        state.cart.push({
+          ...action.payload,
+          quantity: 1,
+          totalPricePerItem: action.payload.cost,
+        });
+      }
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
+    removeItem: (state, action) => {},
+    updateQuantity: (state, action) => {},
   },
 });
 
